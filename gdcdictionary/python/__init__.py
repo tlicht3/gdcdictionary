6b98251fc0180a8433cf0b0176695dc26f070f4a
Joshua Miller <jsmiller@uchicago.edu>
2016-02-22 13:05:45 -0600

    wip

--- gdcdictionary/python/__init__.py
+++ gdcdictionary/python/__init__.py
@@ -1,26 +1,87 @@
-import yaml
-import os
-import glob
+from jsonschema import RefResolver
 
-MOD_DIR = os.path.abspath(os.path.dirname(__file__))
+import glob
+import logging
+import os
+import re
+import yaml
+
+MOD_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 
 
 class GDCDictionary(object):
 
-    def __init__(self, root_dir=os.path.join(
-            os.path.dirname(MOD_DIR), 'schemas'), lazy=False):
-        self.root_dir = root_dir
-        self.metaschema_path = 'metaschema.yaml'
-        self.definitions_path = '_definitions.yaml'
-        self.exclude = [self.metaschema_path, self.definitions_path]
+    _metaschema_path = 'metaschema.yaml'
+    _definitions_paths = [
+        '_definitions.yaml',
+        '_terms.yaml',
+    ]
+
+    logger = logging.getLogger("GDCDictionary")
+
+    def __init__(self, lazy=False, root_dir=None, definitions_paths=None,
+                 metaschema_path=None):
+        """Creates a new dictionary instance.
+
+        :param root_dir: The directory to find schemas
+        :param metaschema_path: The metaschema to validate schemas with
+        :param definitions_paths: Paths to resolve $ref to
+        :param lazy: If true, wait to load dictionary
+
+        """
+
+        self.root_dir = (root_dir or os.path.join(MOD_DIR, 'schemas'))
+        self.metaschema_path = metaschema_path or self._metaschema_path
+        self.definitions_paths = definitions_paths or self._definitions_paths
+        self.exclude = [self.metaschema_path] + self.definitions_paths
         self.schema = dict()
         if not lazy:
             self.load()
 
     def load(self):
+        """Load and reslove all schemas"""
+
         self.metaschema = self.load_yaml_schema(self.metaschema_path)
-        self.definitions = self.load_yaml_schema(self.definitions_path)
+        self.definitions = {}
+        self.resolvers = {
+            '{}'.format(path):
+            RefResolver('{}#'.format(path), self.load_yaml_schema(path))
+            for path in self.definitions_paths
+        }
         self.load_root_dir()
+
+        map(self.resolve_all, self.schema.values())
+
+    def resolve_all(self, root, doc=None):
+        doc = doc or root
+
+        if isinstance(root, dict):
+            modified = False
+            for k, v in root.items():
+                print(k)
+                if k == '$ref':
+
+                    print(k, v)
+                    base, ref = v.split('#')
+                    ref = ref.replace('/', '')
+                    print(base, ref)
+
+                    if base == '':
+                        resolver = RefResolver('#', doc)
+                    else:
+                        resolver = self.resolvers[base]
+
+                    referrer, resolution = resolver.resolve(v)
+                    root.pop(k)
+                    root.update(resolution)
+                    modified = True
+
+            for k, v in root.items():
+                self.resolve_all(k, doc)
+
+        elif isinstance(root, list):
+            for item in root:
+                self.resolve_all(root, doc)
 
     def load_root_dir(self):
         cdir = os.getcwd()
